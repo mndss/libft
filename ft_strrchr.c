5d1377163db4d1559739024febc16a2d0a994a4a
dumendes99 <dum3ndes@gmail.com>
2021-05-26 13:06:29 -0300

    arrumando strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,13 +6,16 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/23 19:08:03 by elima-me          #+#    #+#             */
-/*   Updated: 2021/05/23 19:14:46 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/05/26 13:05:04 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	ft_strrchr(const char *s, int c)
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
@@ -20,8 +23,10 @@ char	ft_strrchr(const char *s, int c)
 	while (i > 0)
 	{
 		if (s[i] == c)
-			return (s[i]);
+			return ((char *)s + i);
 		i--;
 	}
+	if (s[i] == c)
+		return ((char *)s + i);
 	return ('\0');
 }
