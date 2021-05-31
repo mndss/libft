30e821bb289d97b42e64dbffc52fd2417772eb5f
dumendes99 <dum3ndes@gmail.com>
2021-05-31 14:44:46 -0300

    fix substring

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 14:58:00 by elima-me          #+#    #+#             */
-/*   Updated: 2021/05/31 14:20:08 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/05/31 14:41:12 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,8 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
-	size_t	i;
-
-	i = 0;
-	sub = (char *)malloc(sizeof(char) * len + 1);
-	while (i < len)
-	{
-		sub[i] = s[start];
-		start++;
-		i++;
-	}
-	sub[start] = '\0';
+	
+	sub = (char *)malloc(sizeof(char) * len + 1); 
+	ft_strlcpy(sub, &s[start], len + 1);
 	return (sub);
 }
