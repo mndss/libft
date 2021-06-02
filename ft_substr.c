a41a42636d68dd5e53cc9bff34d6312afa90fe73
dumendes99 <dum3ndes@gmail.com>
2021-06-01 23:03:17 -0300

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 14:58:00 by elima-me          #+#    #+#             */
-/*   Updated: 2021/06/01 15:05:48 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/06/01 23:01:33 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	size_s;
 	size_t	max_len;
 
+	size_s = ft_strlen(s);
 	if (s == NULL)
 		return (NULL);
-	sub = (char *)ft_calloc(sizeof(char), len + 1);
-	size_s = ft_strlen(s);
-	max_len = size_s - start;
-	if (!sub)
-		return (NULL);
-	if (len > max_len)
-		len = max_len;
 	if (size_s < start)
 	{
-		*sub = '\0';
+		sub = (char *)ft_calloc(sizeof(char), 1);
+		if (!sub)
+			return (NULL);
 		return (sub);
 	}
+	max_len = size_s - start;
+	if (len > max_len)
+		len = max_len;
+	sub = (char *)ft_calloc(sizeof(char), len + 1);
+	if (!sub)
+		return (NULL);
 	ft_strlcpy(sub, &s[start], len + 1);
 	return (sub);
 }
