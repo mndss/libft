405baf4a158636b6e1d1701a65acb68b3d79abac
dumendes99 <dum3ndes@gmail.com>
2021-06-07 15:07:38 -0300

    fix bonus

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 17:56:41 by elima-me          #+#    #+#             */
-/*   Updated: 2021/06/07 13:21:50 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/06/07 15:05:38 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*last_element;
+	t_list *last_element;
 
-	last_element = *lst;
-	if (lst == NULL || new == NULL)
-		return ;
-	while (last_element->next != NULL)
-		last_element = last_element->next;
-	last_element = new;
-	new-> next = NULL;
-	return ;
+	last_element = ft_lstlast(*lst);
+	if (last_element != NULL)
+		last_element->next = new;
+	else
+		*lst = new;
 }
--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: elima-me <elima-me@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 17:50:03 by elima-me          #+#    #+#             */
-/*   Updated: 2021/06/05 17:53:02 by elima-me         ###   ########.fr       */
+/*   Updated: 2021/06/07 14:55:37 by elima-me         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	while (lst != NULL)
 	{
+		if (lst->next == NULL)
+			return (lst);
 		lst = lst->next;
 	}
-	return (lst);
+	return (NULL);
 }
